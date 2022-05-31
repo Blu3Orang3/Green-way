eb166e3a30669919f39ebe06d70006b073f653b3
Blu3Orang3 <matealtunashvili@gmail.com>
2022-06-01 00:24:35 +0400

    add hero and milestone panel

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-///////////////////////////////////////////////////////////
-// Sticky navigation
 
+//////////////////////////////////////////////////////////
+// Sticky navigation
 const header = document.querySelector("header");
 const navbar = document.querySelector(".nav-wrapper");
-const logo = document.querySelector(".main-logo")
+const logo = document.querySelector(".main-logo");
 
 const obs = new IntersectionObserver(
   function (entries) {
@@ -22,7 +22,7 @@ const obs = new IntersectionObserver(
     // In the viewport
     root: null,
     threshold: 0,
-    rootMargin: "-300px",
+    rootMargin: "-150px",
   }
 );
 obs.observe(header);
@@ -30,10 +30,11 @@ obs.observe(header);
 // animate car
 let tml = gsap.timeline({repeat:-1});
 let start = 0;
-let end = 320;
-tml.fromTo(".car-logo", {x:start}, {duration:8, delay:2, x:end, ease:'none'})
+let end = `25rem`;
+tml.fromTo(".car-logo", {x:start}, {duration:4, delay:2, x:end, ease:'none'})
 .to(".car-logo", {duration:0.2, delay:1,scaleX:-1, ease:'none'})
 .to(".car-logo", {duration:0.3, delay:0.5, rotation:30})
 .to(".car-logo", {duration:0.3, delay:0.5, rotation:0})
-.to(".car-logo", {duration:8, delay:0.5, x:start, ease:'none'})
+.to(".car-logo", {duration:3, delay:0.5, x:start, ease:'none'})
 .to(".car-logo", {duration:0.2, delay:1,scaleX:1, ease:'none' });
+
